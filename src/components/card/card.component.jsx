f874e1e3d7211f618381730b4ab6c47bed823dfa
yourwpmadesimple <wayne@yourwpmadesimple.com>
2021-11-17 04:22:14 -0700

    adding files for github pages

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./card.styles.css";
+
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <img
+        alt="monster"
+        src={`https://robohash.org/${props.monsters.id}?set=set2&size=180x180`}
+      />
+      <h2>{props.monsters.name}</h2>
+      {props.monsters.email}
+    </div>
+  );
+};
